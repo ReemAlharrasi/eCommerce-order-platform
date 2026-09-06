f38273dce855e0f21a486a3791dabfe5a35ff2db
unknown <reemalharrasi00@gmail.com>
2026-09-06 08:52:01 +0400

    spring boot initialised file]

---
+++ eCommerceOrderPlatform/src/main/java/eCommerceOrderPlatform/ECommerceOrderPlatformApplication.java
@@ -0,0 +1,13 @@
+package eCommerceOrderPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOrderPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceOrderPlatformApplication.class, args);
+	}
+
+}
---
+++ eCommerceOrderPlatform/src/test/java/eCommerceOrderPlatform/ECommerceOrderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package eCommerceOrderPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceOrderPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
