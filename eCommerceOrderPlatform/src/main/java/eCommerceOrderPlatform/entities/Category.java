7675e386caf8e8d1781a86ffe3554fb4765440b2
unknown <reemalharrasi00@gmail.com>
2026-09-06 09:33:35 +0400

    created category class

---
+++ eCommerceOrderPlatform/src/main/java/eCommerceOrderPlatform/entities/Category.java
@@ -0,0 +1,18 @@
+package eCommerceOrderPlatform.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category extends BaseClass{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    private String description;
+}
