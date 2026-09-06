eb13455bba6072b26c09fd5c629574c88e401c9a
unknown <reemalharrasi00@gmail.com>
2026-09-06 09:34:28 +0400

    added @generatedValue

--- eCommerceOrderPlatform/src/main/java/eCommerceOrderPlatform/entities/Store.java
+++ eCommerceOrderPlatform/src/main/java/eCommerceOrderPlatform/entities/Store.java
@@ -2,6 +2,7 @@ package eCommerceOrderPlatform.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,7 +12,7 @@ import lombok.Setter;
 @Entity
 public class Store extends BaseClass{
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String name;
     private String location;
 }
